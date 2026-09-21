a65abeecb16d093efedad0f9f80720188c8e019b
christopher Dikesa <chrismsmr@gmail.com>
2026-09-21 15:10:12 +0100

    Add AgentDisconnectedException to imports and exports

--- agentguard/__init__.py
+++ agentguard/__init__.py
@@ -1,22 +1,16 @@
 from .models import (
     RiskLevel, SecurityAction, DetectionConfidence,
-    SecurityCheck, SecurityException, ApprovalRequiredException,
+    SecurityCheck, SecurityException, ApprovalRequiredException, AgentDisconnectedException,
     GuardSpan, RuntimeRiskDecision, TrajectoryEvent
 )
 from .policy import PolicyEngine
 from .sdk import AgentGuard
 
 
-# NOUVEAU : Exception pour le workflow d'approbation humaine
-class ApprovalRequiredException(Exception):
-    def __init__(self, message: str, approval_id: str, details: dict):
-        super().__init__(message)
-        self.approval_id = approval_id
-        self.details = details
-
 __all__ = [
     "RiskLevel", "SecurityAction", "DetectionConfidence",
-    "SecurityCheck", "SecurityException", "ApprovalRequiredException",
+    "SecurityCheck", "SecurityException", "ApprovalRequiredException", "AgentDisconnectedException",
     "GuardSpan", "RuntimeRiskDecision", "TrajectoryEvent",
     "PolicyEngine", "AgentGuard"
 ]
+
